d94e465954c7b004b3c9f84937cada5050896dc7
jsonaguas <jaguas43@gmail.com>
2024-05-29 15:51:33 -0500

    upload

---
+++ Problem 3.py
@@ -0,0 +1,23 @@
+
+variable_a = "Hello, World!"
+print(type(variable_a))
+#Prediction:string
+#Result:str
+
+#####
+variable_b = 23
+print(type(variable_b))
+#Prediction:integer
+#Result:int
+
+#####
+variable_c = 3.14
+print(type(variable_c))
+#Prediction:float
+#Result:float
+#####
+
+variable_d = True
+print(type(variable_d))
+#Prediction:boolean
+#Result:bool
---
+++ Problem 5.py
@@ -0,0 +1,15 @@
+#Task 1
+eqn1=4+5*2
+print(eqn1)
+eqn2=(4+5)*2
+print(eqn2)
+if eqn1==eqn2:
+    print("True")
+else:
+     print("False")
+
+
+#Task 2
+if 5 + 1 > 8 or 4 < 5:
+    print("True")
+#Prediction:true
